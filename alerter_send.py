2ec0b7353a5fab5da95bc254ee634fee0028c507
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-11-28 16:13:02 +0530

    Create alerter_send.py

---
+++ alerter_send.py
@@ -0,0 +1,8 @@
+
+
+def network_alert_stub(celcius):
+    print(f'ALERT: Temperature is {celcius} celcius')
+    # Return 200 for ok
+    # Return 500 for not-ok
+    # stub always succeeds and returns 200
+    return celcius
