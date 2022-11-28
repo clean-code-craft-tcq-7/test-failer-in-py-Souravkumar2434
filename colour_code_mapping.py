832b8d20480063cbb07d0d631bf33280a3dac7d9
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-11-28 15:55:15 +0530

    Create colour_code_mapping.py

---
+++ colour_code_mapping.py
@@ -0,0 +1,12 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+def get_pair_number(majorcolor, minorcolor):
+     return major_colors.index(majorColor) * 5 + minor_colors.index(minorColor)
+     
+
+def print_color_map():
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            print(f' {get_pair_number(major, minor)} | {major} | {minor}')
+    return len(major_colors) * len(minor_colors)
