93a66de389f824f7f64530ab36f387611f12efb1
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-06 16:11:17 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -3,6 +3,6 @@ from alertincelcius import alert_in_celcius, alert_failure_count
 alert_in_celcius(210.2)
 alert_in_celcius(212)
 alert_in_celcius(213)
-print(f'{alertFailureCount} alerts send failed.')
-assert alertFailureCount > 0, f'alert fail count not incremented {alertFailureCount} times.'
+print(f'{alert_failure_count} alerts send failed.')
+assert alert_failure_count > 0, f'alert fail count not incremented {alert_failure_count} times.'
 print("All is well!!")
