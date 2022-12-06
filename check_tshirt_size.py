854bcea3891c80503ecd20357ee25f63d662f33c
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-06 15:39:37 +0530

    Update check_tshirt_size.py

--- check_tshirt_size.py
+++ check_tshirt_size.py
@@ -4,6 +4,7 @@ def size(cms):
     elif cms > 38 and cms < 42:
         return 'M'
     elif cms == 38:
+        print("No information available for size 38cms!")
         return ' '
     else:
         return 'L'
