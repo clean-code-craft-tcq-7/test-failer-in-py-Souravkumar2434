aeac4b2309208658ef828053cffa1bce0f6de520
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-06 14:45:27 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -3,4 +3,4 @@ from colour_code_mapping import print_color_map, get_pair_number
 result = print_color_map()
 assert(result == 25)
 assert get_pair_number("Red","Blue") == 6
-assert get_pair_number("Black","Blue") == 13
+assert get_pair_number("Black","Blue") == 11
