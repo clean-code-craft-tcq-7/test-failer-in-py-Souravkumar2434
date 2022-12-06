86da4ca720a4e40c07a249d5d76cabf28d388389
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-06 14:41:13 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -1,7 +1,7 @@
 from check_tshirt_size import size
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
-assert(size(38) == '')
-assert(size(42) == 'L')
+assert (size(37) == 'S')
+assert (size(40) == 'M')
+assert (size(43) == 'L')
+assert (size(38) == '')
+assert (size(42) == 'L')
